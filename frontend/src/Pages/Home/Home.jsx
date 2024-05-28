d63136d28508e7dbb50e41c8631b5a0e4bc2d097
Abubakar kehinde <abubakarkehinde88@gmail.com>
2024-05-28 14:31:48 +0100

    hero-section

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
+import Footer from '../../components/Footer/Footer'
+import Faqs from '../../components/Faqs/Faqs'
+import Hero from '../../components/Hero/Hero'
 
 const Home = () => {
   return (
     <div>
         <div>
             <Navbar/>
+        </div>
+        <div>
+          <Hero />
+        </div>
+        <div>
+          <Footer />
+        </div>
+        <div>
+          <Faqs />
         </div>
     </div>
   )
