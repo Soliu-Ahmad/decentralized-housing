f412967c4e9e8afdae4fb7c92132b559aceab19b
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2024-05-28 17:11:47 +0100

    3rd commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import Home from "./Pages/Home/Home"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Blog from "./components/Content/blogs/pages/Blog"
+import Marketplace from "./components/Marketplace/Marketplace"
+import Footer from "./components/Footer/Footer"
 
 function App() {
 
@@ -8,7 +10,9 @@ function App() {
    <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home/>}/>
-      <Route path="/blogs" element={<Blog/>}/>
+      {/* <Route path="/blogs" element={<Blog/>}/> */}
+      {/* <Route path="/marketplace" element={<Marketplace/>}/> */}
+      <Route path="/footer" element={<Footer/>}/>
 
     </Routes>
    </BrowserRouter>
--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook, faTwitter, faInstagram } from "@fortawesome/free-brands-svg-icons";
+import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="footerHeader">
+      <div className="first-footer">
+        <h1>
+          Get our latest articles, <br /> offers and news first hand
+        </h1>
+        <form action="#" method="post" className="subscribe-form">
+          <input
+            type="email" 
+            name="email"
+            id="email"
+            placeholder="Enter your email"
+            required
+          />
+          <button type="submit">Subscribe</button>
+        </form>
+      </div>
+      <div className="footer">
+        <div className="footer-info">
+          <div className="contact">
+            <h1>Contact</h1>
+            <p>Phone: (123) 456-7890</p>
+            <p>Address: 123 Main St, Anytown, USA</p>
+            <p>Email: info@example.com</p>
+            <div className="social-links">
+              <a href="#" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faFacebook} />
+              </a>
+              <a href="#" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faTwitter} />
+              </a>
+              <a href="#" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faInstagram} />
+              </a>
+            </div>
+          </div>
+          <div className="links">
+            <h1>Quick Links</h1>
+            <a href="/faqs">FAQs</a>
+            <a href="/properties">Properties</a>
+            <a href="/terms">Terms of Service</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
