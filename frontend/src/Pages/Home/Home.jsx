eafc6ffeede216e2137c1f42bb8948658a53a9a7
Soliu-Ahmad <102731648+Soliu-Ahmad@users.noreply.github.com>
2024-05-28 17:10:42 +0100

    third-commit

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,31 +1,34 @@
-import React from 'react'
-import Navbar from '../../components/Navbar/Navbar'
-import Footer from '../../components/Footer/Footer'
-import Faqs from '../../components/Faqs/Faqs'
-import Hero from '../../components/Hero/Hero'
-import Content from '../../components/Content/Content'
+import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import Footer from "../../components/Footer/Footer";
+import Faqs from "../../components/Faqs/Faqs";
+import Hero from "../../components/Hero/Hero";
+import Content from "../../components/Content/Content";
+import Marketplace from "../../components/Marketplace/Marketplace";
 
 const Home = () => {
   return (
     <div>
-        <div>
-            <Navbar/>
-        </div>
-        <div>
-          <Hero />
-        </div>
-        <div>
-          <Footer />
-        </div>
-        <div>
-          <Faqs />
-        </div>
-         </div>
-         <div>
-            <Content />
-         </div>
+      <div>
+        <Navbar />
+      </div>
+      <div>
+        <Hero />
+      </div>
+      <div>
+        <Content />
+      </div>
+      <div>
+      <Marketplace />
+      </div>
+      <div>
+        <Faqs />
+      </div>
+      <div>
+        <Footer />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
---
+++ frontend/src/components/Marketplace/Marketplace.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import './Marketplace.css';
+import { FaMapMarkerAlt, FaBed, FaShieldAlt } from 'react-icons/fa';
+
+const Marketplace = () => {
+  return (
+    <div className="blogs-container">
+      <div className="title">Market Place</div> 
+      
+      {[1, 2, 3, 4, 5, 6].map((item, index) => (
+        <div className="blog-post" key={index}>
+          <div className="image-container">
+            <img src="hom1.jpeg" alt="Blog" />
+          </div>
+          <div className="blog-title">10 things to know</div>
+          <div className="blog-location">
+            <FaMapMarkerAlt /> Location Name
+          </div>
+          <div className="blog-features">
+            <div><FaBed /> Two Bed</div>
+            <div><FaBed /> Single Room</div>
+            <div><FaShieldAlt /> Security</div>
+          </div>
+          <div className="blog-price">
+            $1500
+          </div>
+          <div className="read-more">
+            <button>Continue Reading</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Marketplace;
