21526f5232426123d7e0cec9f9d6dd6593e4ac0d
Soliu-Ahmad <102731648+Soliu-Ahmad@users.noreply.github.com>
2024-05-28 11:36:34 +0100

    second-commit

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
+import Content from '../../components/Content/Content'
 
 const Home = () => {
   return (
     <div>
         <div>
             <Navbar/>
-        </div>
+         </div>
+         <div>
+            <Content />
+         </div>
     </div>
   )
 }
